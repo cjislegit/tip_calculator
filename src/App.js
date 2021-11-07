970d03f4df07086476e434a5b3c4f787e655dcb5
cjislegit <carlosjramirez11@gmail.com>
2021-11-07 15:49:59 -0800

    Set up app and styled app component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
 import './App.css';
+import logo from './assets/logo.svg';
 
 function App() {
   return (
     <div className='App'>
-      <header className='App-header'>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='pageLogo'>
+        <img src={logo} alt='Spillter Logo' />
+      </div>
+      <div className='calculator'></div>
     </div>
   );
 }
