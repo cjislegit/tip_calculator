f86f9f9b93f3b9e16dd38f6a2e56bc3bd927801c
cjislegit <carlosjramirez11@gmail.com>
2021-11-09 20:27:02 -0800

    Styled the slectTip component

--- src/SelectTip.js
+++ src/SelectTip.js
@@ -3,8 +3,19 @@ import styles from './SelectTip.module.css';
 const SelectTip = () => {
   return (
     <div className={styles.selectTip}>
-      <div className={styles.selectTipLabel}>Select Tip</div>
-      <div className={styles.selectTipContainer}></div>
+      <div className={styles.selectTipLabel}>Select Tip %</div>
+      <div className={styles.selectTipContainer}>
+        <div className={styles.selectTipRowR}>
+          <input type='button' value='5%' />
+          <input type='button' value='15%' />
+          <input type='button' value='50%' />
+        </div>
+        <div className={styles.selectTipRowL}>
+          <input type='button' value='10%' />
+          <input type='button' value='25%' />
+          <input type='text' placeholder='Custom' />
+        </div>
+      </div>
     </div>
   );
 };
