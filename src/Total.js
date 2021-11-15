f516dbd446526f9ba9775ac1dcca4af5209a2e43
Carlos Ramirez <carlosjramirez11@gmail.com>
2021-11-15 15:36:27 -0800

    Started styling the total component

--- src/Total.js
+++ src/Total.js
@@ -2,18 +2,20 @@ import styles from './Total.module.css';
 
 const Total = () => {
   return (
-    <div className='total'>
+    <div className={styles.total}>
       <div className={styles.totalTipAmount}>
-        <div>
-          Tip Amount <br />/ person
+        <div className={styles.totalLeft}>
+          <div className={styles.totalTop}>Tip Amount</div>
+          <div className={styles.totalBottom}>/ person</div>
         </div>
-        <div>$4.27</div>
+        <div className={styles.totalRight}>$4.27</div>
       </div>
       <div className={styles.toalAmount}>
-        <div>
-          Total <br />/ person
+        <div className={styles.totalLeft}>
+          <div className={styles.totalTop}>Total</div>
+          <div className={styles.totalBottom}>/ person</div>
         </div>
-        <div>$32.79</div>
+        <div className={styles.totalRight}>$32.79</div>
       </div>
       <button className={styles.totalReset} value='5'>
         RESET
