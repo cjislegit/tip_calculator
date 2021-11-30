12f5768fe7edd445f1001485a59e0d7ada79a937
Carlos Ramirez <carlosjramirez11@gmail.com>
2021-11-29 19:36:18 -0800

    SelectTup component is now more responsive

--- src/SelectTip.js
+++ src/SelectTip.js
@@ -5,34 +5,30 @@ const SelectTip = ({ setTipAmount }) => {
     <div className={styles.selectTip}>
       <div className={styles.selectTipLabel}>Select Tip %</div>
       <div className={styles.selectTipContainer}>
-        <div className={styles.selectTipRowR}>
-          <button
-            value='5'
-            autoFocus
-            onClick={(e) => setTipAmount(e.target.value)}
-          >
-            5%
-          </button>
-          <button value='15' onClick={(e) => setTipAmount(e.target.value)}>
-            15%
-          </button>
-          <button value='50' onClick={(e) => setTipAmount(e.target.value)}>
-            50%
-          </button>
-        </div>
-        <div className={styles.selectTipRowL}>
-          <button value='10' onClick={(e) => setTipAmount(e.target.value)}>
-            10%
-          </button>
-          <button value='25' onClick={(e) => setTipAmount(e.target.value)}>
-            25%
-          </button>
-          <input
-            type='text'
-            placeholder='Custom'
-            onChange={(e) => setTipAmount(e.target.value)}
-          />
-        </div>
+        <button
+          value='5'
+          autoFocus
+          onClick={(e) => setTipAmount(e.target.value)}
+        >
+          5%
+        </button>
+        <button value='10' onClick={(e) => setTipAmount(e.target.value)}>
+          10%
+        </button>
+        <button value='15' onClick={(e) => setTipAmount(e.target.value)}>
+          15%
+        </button>
+        <button value='25' onClick={(e) => setTipAmount(e.target.value)}>
+          25%
+        </button>
+        <button value='50' onClick={(e) => setTipAmount(e.target.value)}>
+          50%
+        </button>
+        <input
+          type='text'
+          placeholder='Custom'
+          onChange={(e) => setTipAmount(e.target.value)}
+        />
       </div>
     </div>
   );
